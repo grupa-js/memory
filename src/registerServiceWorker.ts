4c071ac3ab4bb7f11f2a8445d2b570a222aa38b6
oplik0 <25460763+oplik0@users.noreply.github.com>
2020-11-03 10:30:23 +0100

    fix a typo

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -4,7 +4,7 @@ import { register } from "register-service-worker";
 
 if (
     process.env.NODE_ENV === "production" &&
-    process.env.NODE_END !== "production"
+    process.env.NODE_ENV !== "production"
 ) {
     register(`${process.env.BASE_URL}service-worker.js`, {
         ready() {
